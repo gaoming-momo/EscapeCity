755b55eea5b45664d996a545eccc2ac1a255b63a
gaoming <1064308261@qq.com>
2022-06-06 22:03:48 +0800

    完善登录接口

--- src/main/java/com/tencent/wxcloudrun/constant/Common.java
+++ src/main/java/com/tencent/wxcloudrun/constant/Common.java
@@ -2,6 +2,5 @@ package com.tencent.wxcloudrun.constant;
 
 public class Common {
     public static final String APP_ID = "wxa5b68caf37fa9d2d";
-    public static final String secretId = "AKIDe9TcXoyuc9UaHsGPJEuMZXqyrVv63Vkk";
-    public static final String secretKey = "oOkr5FCwSlngV4TikZBZjfNq43nGWZP3";
+    public static final String secretKey = "8e34a5f349f11c526d6a06c8dfeb751d";
 }
--- src/main/java/com/tencent/wxcloudrun/controller/LoginController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/LoginController.java
@@ -1,10 +1,12 @@
 package com.tencent.wxcloudrun.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.constant.Common;
+import com.tencent.wxcloudrun.utils.HttpUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,7 +41,7 @@ public class LoginController {
     //小程序唯一标识   (在微信小程序管理后台获取)
     String wxspAppid = Common.APP_ID;
     //小程序的 app secret (在微信小程序管理后台获取)
-    String wxspSecret = Common.secretId;
+    String wxspSecret = Common.secretKey;
     //授权（必填）
     String grant_type = "authorization_code";
     String url = "https://api.weixin.qq.com/sns/jscode2session";
@@ -57,5 +59,24 @@ public class LoginController {
     logger.error("response:{}",response);
     //解析相应内容（转换成json对象）
     return ApiResponse.ok(response);
+  }
+
+  public static void main(String[] args) throws IOException {
+    //小程序唯一标识   (在微信小程序管理后台获取)
+    String wxspAppid = Common.APP_ID;
+    //小程序的 app secret (在微信小程序管理后台获取)
+    String wxspSecret = Common.secretKey;
+    //授权（必填）
+    String grant_type = "authorization_code";
+    String url = "https://api.weixin.qq.com/sns/jscode2session";
+
+    String code = "07116v000EhIYN1NLp100SXDUp216v0m";
+    //https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code
+    //1、向微信服务器 使用登录凭证 code 获取 session_key 和 openid
+    //请求参数
+    String params = "appid=" + wxspAppid + "&secret=" + wxspSecret + "&js_code=" + code + "&grant_type=" + grant_type;
+    params = params.trim();
+    String sr = HttpUtils.sendGet("https://api.weixin.qq.com/sns/jscode2session", params);
+    System.out.println("response = " + sr);
   }
 }
